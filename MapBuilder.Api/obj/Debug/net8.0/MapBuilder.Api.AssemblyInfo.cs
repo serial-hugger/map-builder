7a79bfb8d22d68c5203d568cab369aae20393068
serial-hugger <serialhugger13@gmail.com>
2024-11-27 22:14:15 -0500

    Preparing to add ignore file

--- MapBuilder.Api/obj/Debug/net8.0/MapBuilder.Api.AssemblyInfo.cs
+++ MapBuilder.Api/obj/Debug/net8.0/MapBuilder.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapBuilder.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b5d5b8f29147ad29f7b392d4415681b6be4962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc8a3573cf8c02ba8caff3208be0cc12dde8471")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapBuilder.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapBuilder.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MapBuilder.Tests/obj/Debug/net8.0/MapBuilder.Tests.AssemblyInfo.cs
+++ MapBuilder.Tests/obj/Debug/net8.0/MapBuilder.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapBuilder.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782996c471ecfdbf57e92eccfdc2a590d5feb727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc8a3573cf8c02ba8caff3208be0cc12dde8471")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapBuilder.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapBuilder.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
